926440174f6dc1fe18bc444a30b8dd3d33b591e2
Dave Brosius <dbrosius@mebigfatguy.com>
2019-01-11 23:11:52 -0500

    Happy New Year

--- src/com/mebigfatguy/loc4j/Counts.java
+++ src/com/mebigfatguy/loc4j/Counts.java
@@ -1,7 +1,7 @@
 /*
  * loc4j - a source code line counting ant task
- * Copyright 2016 MeBigFatGuy.com
- * Copyright 2016 Dave Brosius
+ * Copyright 2016-2019 MeBigFatGuy.com
+ * Copyright 2016-2019 Dave Brosius
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
